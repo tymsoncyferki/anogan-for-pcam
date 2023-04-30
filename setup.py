9c287463f59e294da322bfd9ec00ca9ea1edf668
Tymoteusz Urban <115067539+tymsoncyferki@users.noreply.github.com>
2023-04-30 09:06:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="A03ki",
     python_requires=">=3.6",
     install_requires=["torch", "torchvision", "torchvision4ad", "matplotlib",
-                      "numpy", "pandas", "Pillow", "scikit-learn"],
+                      "numpy", "pandas", "Pillow", "scikit-learn", "h5py"],
     url="https://github.com/A03ki/f-AnoGAN",
     license="MIT License",
     packages=["fanogan"]
